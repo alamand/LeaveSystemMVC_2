5fc8c127b7f9bf45ae5bf330a1f8ec0aab194e45
Hamza Rahimy <Hamza Rahimy>
2016-10-27 00:34:07 +0400

    Added froms, views, and controllers for the required pages

---
+++ LeaveSystemMVC/Models/UpdateStatusSelectionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveSystemMVC.Models
+{
+    public struct SMDetails_LUSShort
+    {
+        public string name;
+        public DateTime startDate;
+        public DateTime endDate;
+    }
+    public class UpdateStatusSelectionModel
+    {
+        public SMDetails_LUSShort[] updatableStaffMembers;
+    }
+}
