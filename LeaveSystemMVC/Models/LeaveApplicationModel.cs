5e422c3a549283210c8a57199a695aaffead20a4
Hamza Rahimy <Hamza Rahimy>
2016-10-26 08:19:45 -0700

    added 3 models and views

---
+++ LeaveSystemMVC/Models/LeaveApplicationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveSystemMVC.Models
+{
+    public class LeaveApplicationModel
+    {
+        public string leaveType { get; set; }
+        public DateTime date { get; set; }
+        public DateTime endDate { get; set; }
+        public DateTime returnDate { get; set; }
+        public int lShortStartHour { get; set; }
+        public int lShortStartMinute { get; set; }
+        public int lShortEndHour { get; set; }
+        public int lShortEndMinute { get; set; }
+
+        public string comments { get; set; }
+        public string supportingDocs { get; set; }
+        public bool bookAirTicket { get; set; }
+    }
+}
---
+++ LeaveSystemMVC/Models/LeaveStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveSystemMVC.Models
+{
+    public class LeaveStatus
+    {
+        public DateTime formSubmitDate { get; set; }
+        public string status { get; set; }
+        public DateTime leaveStartDate { get; set; }
+        public DateTime leaveEndDate { get; set; }
+        public string Form { get; set; }
+        public string supportingDocument { get; set; }
+    }
+}
