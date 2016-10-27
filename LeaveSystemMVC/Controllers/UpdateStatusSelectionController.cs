b2af60c47bb98025f5d3221d1dac10a2a5975c38
sukhisinghsidhu <sukhpreets.sidhu@gmail.com>
2016-10-27 15:50:45 +0400

    comminting changes for jasim

--- LeaveSystemMVC/Controllers/UpdateStatusSelectionController.cs
+++ LeaveSystemMVC/Controllers/UpdateStatusSelectionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//hamza created a page to filter names and leave dates for applications 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
--- LeaveSystemMVC/Models/lmChangeApplicationStatus.cs
+++ LeaveSystemMVC/Models/lmChangeApplicationStatus.cs
@@ -8,9 +8,9 @@ namespace LeaveSystemMVC.Models
     public class lmChangeApplicationStatus
     {
         public string name { get; set; }
-        public string applicationDate { get; set; }
+        //public string applicationDate { get; set; }
         public string form { get; set; }
-        public string supportingDocs { get; set; }
+        //public string supportingDocs { get; set; }
         public string comments { get; set; }
         public string currentStatus { get; set; }
         public string newStatus { get; set; }
