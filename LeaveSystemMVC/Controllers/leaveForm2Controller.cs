a0ffebf250c9d30281629f6b40a4c39d4a8a872b
FahadMumtaz <FahadMumtaz@LENOVO-PC>
2016-10-27 14:14:16 +0400

    ss

---
+++ LeaveSystemMVC/Controllers/leaveForm2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeaveSystemMVC.Controllers
+{
+    public class leaveFormController : Controller
+    {
+        // GET: leaveForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LeaveSystemMVC/Models/leaveForm2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveSystemMVC.Models
+{
+    public class leaveForm
+    {
+        public string leaveType { get; set; }
+
+
+       // [DataType(DataType.Date)]
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public DateTime returnDate { get; set; }
+        public DateTime startShort { get; set; }
+        public DateTime endShort { get; set; }
+        public int contact { get; set; }
+        public string comment { get; set; }
+        public string document { get; set; }
+        public Boolean airTicket { get; set; }
+
+    }
+}
