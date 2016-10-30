08cb233aec232e084f0e1660596f4e2ecf58655d
zobad16 <zobad.mahmood@gmail.com>
2016-10-30 11:07:52 +0400

    backup reload

---
+++ LeaveSystemMVC/Models/DaysInLieuModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveSystemMVC.Models
+{
+    public class DaysInLieuModel
+    {
+        public string employeeName { get; set; }
+        public int numberOfDays { get; set; }
+    }
+}
