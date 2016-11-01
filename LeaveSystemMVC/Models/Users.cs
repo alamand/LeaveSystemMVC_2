14838e354877ef46a435a8cbc137aa823aac421a
Hamza Rahimy <Hamza Rahimy>
2016-11-02 01:16:45 +0400

    Successfully implemented feature to load UI elements based on user role and Login Feature

---
+++ LeaveSystemMVC/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveSystemMVC.Models
+{
+    public class Users
+    {
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
