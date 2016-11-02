a2591780609655526b9101bff093a1a9617d20b9
Hamza Rahimy <Hamza Rahimy>
2016-11-03 03:31:05 +0400

    Manually merged new user roles/login feature and the updated models

--- LeaveSystemMVC/Global.asax.cs
+++ LeaveSystemMVC/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Helpers;
 using System.Security.Claims;
 
 namespace LeaveSystemMVC
