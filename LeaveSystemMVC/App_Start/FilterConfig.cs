e761ecb4afb017e929a0eaca5a486b1cf40ca764
Hamza Rahimy <Hamza Rahimy>
2016-11-03 03:07:43 +0400

    ss

--- LeaveSystemMVC/App_Start/FilterConfig.cs
+++ LeaveSystemMVC/App_Start/FilterConfig.cs
@@ -1,6 +1,8 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-
 namespace LeaveSystemMVC
 {
     public class FilterConfig
@@ -8,6 +10,7 @@ namespace LeaveSystemMVC
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
