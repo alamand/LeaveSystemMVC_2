e2db60d77b047f89f0b0283ca24d6ccc2219fb8e
Hamza Rahimy <Hamza Rahimy>
2016-11-03 03:19:22 +0400

    ss

--- LeaveSystemMVC/Global.asax.cs
+++ LeaveSystemMVC/Global.asax.cs
@@ -16,6 +16,8 @@ namespace LeaveSystemMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
         }
     }
 }
