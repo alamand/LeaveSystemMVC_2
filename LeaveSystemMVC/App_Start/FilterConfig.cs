47b61a70a48f5e7fcf21024afc01615ce9f0f1e7
sukhisinghsidhu <sukhpreets.sidhu@gmail.com>
2016-11-09 12:01:01 +0400

    Made changes to the admin pages after ma'am's feedback.

--- LeaveSystemMVC/App_Start/FilterConfig.cs
+++ LeaveSystemMVC/App_Start/FilterConfig.cs
@@ -10,7 +10,7 @@ namespace LeaveSystemMVC
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeAttribute());
+            //filters.Add(new AuthorizeAttribute());
         }
     }
 }
