63d9fa2718a892674f08bd2d3817f241517f8acf
Hamza Rahimy <Hamza Rahimy>
2016-11-13 02:37:03 +0400

    Progress on csv parse - need to upload files to database first

--- LeaveSystemMVC/Controllers/aBulkAddStaffController.cs
+++ LeaveSystemMVC/Controllers/aBulkAddStaffController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LeaveSystemMVC.Models;
 
 namespace LeaveSystemMVC.Controllers
 {
     public class aBulkAddStaffController : Controller
     {
+        sEmployeeModel tempEmp = new sEmployeeModel();
+        
+        [HttpGet]
         // GET: aBulkAddStaff
         public ActionResult Index()
         {
-            return View();
+            tempEmp.firstName = "File name here...";
+            return View(tempEmp);
+        }
+        
+
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
