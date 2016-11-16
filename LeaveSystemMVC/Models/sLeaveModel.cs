b4a6abfe461666347103f1deb9d299da673fa392
jasim_4b <jasimkhumi@outlook.com>
2016-11-16 20:46:34 +0400

    model Changed

--- LeaveSystemMVC/Models/sLeaveModel.cs
+++ LeaveSystemMVC/Models/sLeaveModel.cs
@@ -17,53 +17,50 @@ namespace LeaveSystemMVC.Models
         public String leaveType { get; set; } = "Annual";
 
         //[DisplayName("Select Application Date")]
-        [Required(ErrorMessage = "Please select leave application date")]
-        public DateTime applicationDate { get; set; } = new DateTime(24-11-2016);
+        public DateTime applicationDate { get; set; } 
 
         [DisplayName("Select Leave Start Date")]
         [Required(ErrorMessage = "Please select leave start date")]
-        public DateTime startDate { get; set; } = new DateTime(24 - 12 - 2016);
+        public DateTime startDate { get; set; } 
 
         [DisplayName("Select Leave End Date")]
         [Required(ErrorMessage = "Please select leave end date")]
-        public DateTime endDate { get; set; } = new DateTime(24 - 1 - 2017);
+        public DateTime endDate { get; set; } 
 
-        public DateTime returnDate { get; set; } = new DateTime(24 - 1 - 2017);
+        public DateTime returnDate { get; set; }
 
-        [DisplayName("Leave Duration")]
-        [Required(ErrorMessage = "Please enter the leave duration before submitting form")]
-        public int leaveDuration { get; set; } = 30;
+        public int leaveDuration { get; set; } 
 
         [DisplayName("Select Leave Start Time")]
         [Required(ErrorMessage = "Please select leave start time")]
-        public DateTime shortStartTime { get; set; } = new DateTime(24 - 1 - 2017);
+        public DateTime shortStartTime { get; set; }
 
         [DisplayName("Select Leave End Time")]
         [Required(ErrorMessage = "Please select leave end time")]
-        public DateTime shortEndTime { get; set; } = new DateTime(24 - 1 - 2017);
+        public DateTime shortEndTime { get; set; } 
 
         [DisplayName("Line Manager Comment")]
-        public string lmComment { get; set; } = "Leave Approved";
+        public string lmComment { get; set; } 
 
         [DisplayName("HR Comment")]
-        public string hrComment { get; set; } = "Leave Approved";
+        public string hrComment { get; set; } 
 
         [DisplayName("Leave Status")]
-        public int leaveStatus { get; set; } = 1;
+        public int leaveStatus { get; set; } 
 
         [DisplayName("Enter Comments")]
-        public string comments { get; set; } = "Taking Annual Leave";
+        public string comments { get; set; } 
 
         [DisplayName("Upload Supporting Docs")]
-        public string supportingDocs { get; set; } = "/abc";
+        public string supportingDocs { get; set; } 
 
         [DisplayName("Book Air Ticket?")]
-        public bool bookAirTicket { get; set; } = true;
+        public bool bookAirTicket { get; set; } 
 
         [DisplayName("Phone Number")]
         [Required(ErrorMessage = "You must provide your home phone number.")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "You must provide a proper phone number.")]
         [RegularExpression(@"^(?:\+971|00971|0)?(?:50|51|52|55|56|2|3|4|6|7|9)\d{7}$", ErrorMessage = "You must provide a proper phone number.")]
-        public string contactDetails { get; set; } = "052123344";
+        public string contactDetails { get; set; } 
     }
 }
