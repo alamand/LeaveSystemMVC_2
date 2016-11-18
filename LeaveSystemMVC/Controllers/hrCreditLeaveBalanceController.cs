1b8c21e2831e2997acfa976d1b3a796abd08c30f
zobad16 <zobad.mahmood@gmail.com>
2016-11-18 23:09:44 +0400

    Added changes to holiday name and validation

--- LeaveSystemMVC/Controllers/hrCreditLeaveBalanceController.cs
+++ LeaveSystemMVC/Controllers/hrCreditLeaveBalanceController.cs
@@ -21,71 +21,10 @@ namespace LeaveSystemMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-                var list = new List<Leave>();
-                var driver = new List<Leave>();
-                var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-                //var connectionString = ConfigurationManager.ConnectionStrings["CustomConnection"].ConnectionString;
-                string queryString = "Select * from LeaveSystem.dbo.Leave_Type";
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    var command = new SqlCommand(queryString, connection);
-
-                    connection.Open();
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            //if not driver
-                            var id = (int)reader["Leave_ID"];
-                            var duration = (int)reader["Duration"];
-                            list.Add(new Leave(id,duration ));
-                            //if driver CreditDriver()
-                        }
-                    }
-
-                    connection.Close();
-                }
-                foreach (var item in list)
-                  {
-                      CreditBalance(item.id, item.duration);
-                  }
+             //Something
             }
             return View();
         }
-        public struct Leave
-        {
-            public Leave(int a, int b) { id = a; duration = b; }
-            public int id { get; set; }
-            public int duration { get; set; }
-        }
-        public void CreditBalance(int leaveID, int balance)
-        {
-            var model = new List<Models.hrHolidaysCalender>();
-            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            //var connectionString = ConfigurationManager.ConnectionStrings["CustomConnection"].ConnectionString;
-            string queryString = "Update dbo.Leave_Balance SET Balance='"+balance+"' WHERE Leave_ID='"+leaveID+"'";
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var command = new SqlCommand(queryString, connection);
-
-                connection.Open();
-                using (var reader = command.ExecuteReader())
-                {
-                }
-
-                connection.Close();
-            }
-
-        }
-        public bool isDriver(int id)
-        {
-            return false;
-        }
-        public Leave CreditDriver(int id, int duration)
-        {
-            return new Leave(id,duration);
-        }
-
 
     }
 }
