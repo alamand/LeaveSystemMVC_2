ca3598895f6cf313172fc70bdbafd354b6572ac7
Hamed Alhinai <alhinai.hamed@gmail.com>
2017-11-18 16:26:47 +0400

    Added Leave History for Staff Members

--- LeaveSystemMVC/Controllers/sLeaveHistoryController.cs
+++ LeaveSystemMVC/Controllers/sLeaveHistoryController.cs
@@ -16,15 +16,13 @@ namespace LeaveSystemMVC.Controllers
         {
             var model = new List<Models.sLeaveModel>();
             var claimsIdentity = User.Identity as System.Security.Claims.ClaimsIdentity;
-            var c = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
-            ViewBag.claim = c;
-            string a = c.ToString();
-            a = a.Substring(a.Length - 5);
-            //System.Diagnostics.Debug.WriteLine("id is:"+a + ".");
-
+            var identity = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            string loggedInID = identity.ToString();
+            loggedInID = loggedInID.Substring(loggedInID.Length - 5);
+            //System.Diagnostics.Debug.WriteLine("id is:"+ loggedInID + ".");
 
             var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            string query = "Select * FROM dbo.leave,dbo.Leave_Type where Employee_ID = '" + a + "' AND leave.Leave_ID = Leave_Type.Leave_ID and leave.Leave_Status_ID IN (2,3,4,5)";
+            string query = "Select Leave_Application_ID, Leave_Name, Leave_Status_ID, Start_Date, Reporting_Back_Date, Start_Hrs, End_Hrs, Total_Leave_Days FROM dbo.leave,dbo.Leave_Type where Employee_ID = '" + loggedInID + "' AND leave.Leave_ID = Leave_Type.Leave_ID";
 
             using (var connection = new SqlConnection(connectionString)){
                 var command = new SqlCommand(query, connection);
@@ -34,57 +32,22 @@ namespace LeaveSystemMVC.Controllers
                 using (var reader = command.ExecuteReader()) {
                     while (reader.Read()) {
                         var leave = new Models.sLeaveModel();
-                        string leave1 = (string)reader["Leave_Name"];
 
-                        if (leave1.Equals("Annual"))
-                            leave.leaveType = "Annual";
-
-                        if (leave1.Equals("Sick"))
-                            leave.leaveType = "Sick";
-
-                        if (leave1.Equals("Compassionate"))
-                            leave.leaveType = "Compassionate";
-
-                        if (leave1.Equals("Maternity"))
-                            leave.leaveType = "Maternity";
-
-                        if (leave1.Equals("Short_Hours"))
-                            leave.leaveType = "Short";
-
-                        if (leave1.Equals("Unpaid"))
-                            leave.leaveType = "Unpaid";
-
+                        leave.leaveID = reader["Leave_Application_ID"].ToString();
+                        leave.leaveType = (string)reader["Leave_Name"];
                         leave.startDate = (DateTime)reader["Start_Date"];
-
-                        
-                        leave.endDate = (DateTime)reader["End_Date"];
-                        
+                        leave.endDate = (DateTime)reader["Reporting_Back_Date"];
                         leave.leaveDuration = (int)reader["Total_Leave_Days"];
-                        if (!reader.IsDBNull(11))
-                        {
-                            leave.shortStartTime = (TimeSpan)reader["Start_Hrs"];
-                        }else { 
-                        leave.shortStartTime = new TimeSpan(0, 0, 0, 0, 0);
-                        }
-                        if (!reader.IsDBNull(12))
-                        {
-                            leave.shortEndTime = (TimeSpan)reader["End_Hrs"];
-                        }
-                        else
-                        {
-                            leave.shortEndTime = new TimeSpan(0, 0, 0, 0, 0);
-                        }
-                        
+                        leave.shortStartTime = (!DBNull.Value.Equals(reader["Start_Hrs"])) ? (TimeSpan)reader["Start_Hrs"] : new TimeSpan(0, 0, 0, 0, 0);
+                        leave.shortEndTime = (!DBNull.Value.Equals(reader["End_Hrs"])) ? (TimeSpan)reader["End_Hrs"] : new TimeSpan(0, 0, 0, 0, 0);
                         leave.leaveStatus = (int)reader["Leave_Status_ID"];
-
-
                         model.Add(leave);
                     }
                 }
                 connection.Close();
             }
 
-                return View(model);
+        return View(model);
         }
     }
 }
