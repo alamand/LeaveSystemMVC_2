112477fe7f069484b5d3c5f25ff3acd365515d66
Hamed Alhinai <alhinai.hamed@gmail.com>
2017-11-18 14:48:25 +0400

    Clean up

---
+++ LeaveSystemMVC/Models/hrEmpLeaveBalModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveSystemMVC.Models
+{
+    public class hrEmpLeaveBalModel
+    {
+        public sleaveBalanceModel leaveBalance { get; set; }
+        public sEmployeeModel employee { get; set; }
+    }
+}
