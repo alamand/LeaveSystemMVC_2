65db2dcf201271654a0350b69b9ef743b5c82cc4
Mandy <alamand@gmail.com>
2018-02-17 19:07:31 +0400

    Fixed Personal_Email exception

--- LeaveSystemMVC/Controllers/HumanResources/hrEmployeeLeaveHistoryController.cs
+++ LeaveSystemMVC/Controllers/HumanResources/hrEmployeeLeaveHistoryController.cs
@@ -39,7 +39,7 @@ namespace LeaveSystemMVC.Controllers
         private string GetFilteredQuery(int deptID, int accStat, string search, string order, string sDate, string eDate)
         {
             var queryString = "SELECT Leave_Application_ID, Employee.Employee_ID, First_Name, Last_Name, Leave.Start_Date, Leave.Reporting_Back_Date, Leave.Leave_ID, Leave_Name, " +
-                "Contact_Outside_UAE, Comment, Documentation, Flight_Ticket, Total_Leave, Start_Hrs, End_Hrs, Leave.Leave_Status_ID, Status_Name, HR_Comment, LM_Comment " +
+                "Contact_Outside_UAE, Comment, Documentation, Flight_Ticket, Total_Leave, Start_Hrs, End_Hrs, Leave.Leave_Status_ID, Status_Name, HR_Comment, LM_Comment, Leave.Personal_Email " +
                 "FROM dbo.Leave, dbo.Employee, dbo.Leave_Type, dbo.Leave_Status, dbo.Department, dbo.Reporting " +
                 "WHERE Leave.Employee_ID = Employee.Employee_ID AND Leave.Leave_ID = Leave_Type.Leave_ID AND " +
                 "Leave.Leave_Status_ID = Leave_Status.Leave_Status_ID AND Department.Department_ID = Employee.Department_ID AND Employee.Employee_ID = Reporting.Employee_ID " +
